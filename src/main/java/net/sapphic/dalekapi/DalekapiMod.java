2f058826a6e908f23c5345286dbb86e834cfd147
SaphicDeveloper2 <sladesaphire@gmail.com>
2025-09-03 23:21:34 +0100

    DalekAPIV0.1.0.3.4 Pack System

--- src/main/java/net/sapphic/dalekapi/DalekapiMod.java
+++ src/main/java/net/sapphic/dalekapi/DalekapiMod.java
@@ -1,75 +1,160 @@
 package net.sapphic.dalekapi;
 
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
-
-import net.minecraftforge.network.simple.SimpleChannel;
-import net.minecraftforge.network.NetworkRegistry;
-import net.minecraftforge.network.NetworkEvent;
-import net.minecraftforge.fml.util.thread.SidedThreadGroups;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.common.MinecraftForge;
-
+import com.mojang.logging.LogUtils;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.SpawnEggItem;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ModelEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegisterEvent;
+import org.slf4j.Logger;
+import wcore.sapphic.api.easy.client.EasyEntityClient;
+import wcore.sapphic.commands.ReloadPacksCommand;
+import wcore.sapphic.datapack.CybermanManager;
+import wcore.sapphic.datapack.DalekManager;
+import wcore.sapphic.datapack.SonicManager;
+import wcore.sapphic.entities.CybermanEntity;
+import wcore.sapphic.entities.DalekEntity;
+import wcore.sapphic.init.ModEntities;
+import wcore.sapphic.init.ModItems;
+import wcore.sapphic.items.PackagedSonicItem;
+import wcore.sapphic.packs.PackAssetManager;
+import wcore.sapphic.packs.PackLoader;
+import wcore.sapphic.renderers.BaseCybermanRenderer;
+import wcore.sapphic.renderers.BaseDalekRenderer;
+import wcore.sapphic.api.easy.EasyEntityRegistry;
 
-import java.util.function.Supplier;
-import java.util.function.Function;
-import java.util.function.BiConsumer;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.List;
-import java.util.Collection;
-import java.util.ArrayList;
-import java.util.AbstractMap;
 
-@Mod("dalekapi")
+@Mod(DalekapiMod.MODID)
 public class DalekapiMod {
-	public static final Logger LOGGER = LogManager.getLogger(DalekapiMod.class);
-	public static final String MODID = "dalekapi";
+    public static final String MODID = "dalekapi";
+    private static final Logger LOGGER = LogUtils.getLogger();
 
-	public DalekapiMod() {
-		// Start of user code block mod constructor
-		// End of user code block mod constructor
-		MinecraftForge.EVENT_BUS.register(this);
-		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+    public DalekapiMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-		// Start of user code block mod init
-		// End of user code block mod init
-	}
+        LOGGER.info("Initializing PackLoader for DalekAPI...");
+        PackLoader.initialize();
 
-	// Start of user code block mod methods
-	// End of user code block mod methods
-	private static final String PROTOCOL_VERSION = "1";
-	public static final SimpleChannel PACKET_HANDLER = NetworkRegistry.newSimpleChannel(new ResourceLocation(MODID, MODID), () -> PROTOCOL_VERSION, PROTOCOL_VERSION::equals, PROTOCOL_VERSION::equals);
-	private static int messageID = 0;
+        EasyEntityRegistry.init(MODID, modEventBus);
+        ModItems.register(modEventBus);
+        ModEntities.register(modEventBus);
 
-	public static <T> void addNetworkMessage(Class<T> messageType, BiConsumer<T, FriendlyByteBuf> encoder, Function<FriendlyByteBuf, T> decoder, BiConsumer<T, Supplier<NetworkEvent.Context>> messageConsumer) {
-		PACKET_HANDLER.registerMessage(messageID, messageType, encoder, decoder, messageConsumer);
-		messageID++;
-	}
+        modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::addCreative);
 
-	private static final Collection<AbstractMap.SimpleEntry<Runnable, Integer>> workQueue = new ConcurrentLinkedQueue<>();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
 
-	public static void queueServerWork(int tick, Runnable action) {
-		if (Thread.currentThread().getThreadGroup() == SidedThreadGroups.SERVER)
-			workQueue.add(new AbstractMap.SimpleEntry<>(action, tick));
-	}
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            // This is where we can register our virtual datapack
+        });
+    }
 
-	@SubscribeEvent
-	public void tick(TickEvent.ServerTickEvent event) {
-		if (event.phase == TickEvent.Phase.END) {
-			List<AbstractMap.SimpleEntry<Runnable, Integer>> actions = new ArrayList<>();
-			workQueue.forEach(work -> {
-				work.setValue(work.getValue() - 1);
-				if (work.getValue() == 0)
-					actions.add(work);
-			});
-			actions.forEach(e -> e.getKey().run());
-			workQueue.removeAll(actions);
-		}
-	}
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        // Add items to creative tabs here
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ModEventBusEvents {
+        @SubscribeEvent
+        public static void onRegister(RegisterEvent event) {
+            if (event.getRegistryKey().equals(ForgeRegistries.Keys.ITEMS)) {
+                LOGGER.info("Dynamically registering items from packs...");
+                // Register Sonics
+                SonicManager.INSTANCE.getDefinitions().forEach((id, definition) -> {
+                    event.register(ForgeRegistries.Keys.ITEMS, id, () -> new PackagedSonicItem(new Item.Properties().stacksTo(1).rarity(Rarity.RARE), id));
+                });
+                // Register Spawn Eggs
+                DalekManager.INSTANCE.getDefinitions().forEach((id, definition) -> {
+                    int primaryColor = definition.getSpawnEgg().getPrimaryColor();
+                    int secondaryColor = definition.getSpawnEgg().getSecondaryColor();
+                    event.register(ForgeRegistries.Keys.ITEMS, id.withSuffix("_spawn_egg"), () -> new SpawnEggItem(ModEntities.DALEK_ENTITIES.get(id).get(), primaryColor, secondaryColor, new Item.Properties()));
+                });
+                CybermanManager.INSTANCE.getDefinitions().forEach((id, definition) -> {
+                    int primaryColor = definition.getSpawnEgg().getPrimaryColor();
+                    int secondaryColor = definition.getSpawnEgg().getSecondaryColor();
+                    event.register(ForgeRegistries.Keys.ITEMS, id.withSuffix("_spawn_egg"), () -> new SpawnEggItem(ModEntities.CYBERMAN_ENTITIES.get(id).get(), primaryColor, secondaryColor, new Item.Properties()));
+                });
+            }
+
+            if (event.getRegistryKey().equals(ForgeRegistries.Keys.ENTITY_TYPES)) {
+                LOGGER.info("Dynamically registering entities from packs...");
+                DalekManager.INSTANCE.getDefinitions().forEach((id, definition) ->
+                        ModEntities.DALEK_ENTITIES.put(id, EasyEntityRegistry.registerAdvancedMob(
+                                id.getPath(),
+                                definition.getMobCategory(),
+                                0.9f, 1.9f,
+                                DalekEntity::new,
+                                DalekEntity.createAttributes()
+                        ))
+                );
+                CybermanManager.INSTANCE.getDefinitions().forEach((id, definition) ->
+                        ModEntities.CYBERMAN_ENTITIES.put(id, EasyEntityRegistry.registerAdvancedMob(
+                                id.getPath(),
+                                definition.getMobCategory(),
+                                0.6f, 1.95f,
+                                CybermanEntity::new,
+                                CybermanEntity.createAttributes()
+                        ))
+                );
+            }
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            event.enqueueWork(() -> {
+                ModEntities.DALEK_ENTITIES.values().forEach(ro -> EasyEntityClient.registerRenderer(ro, BaseDalekRenderer::new));
+                ModEntities.CYBERMAN_ENTITIES.values().forEach(ro -> EasyEntityClient.registerRenderer(ro, BaseCybermanRenderer::new));
+            });
+        }
+
+        @SubscribeEvent
+        public static void onRegisterModels(ModelEvent.RegisterAdditional event) {
+            // For every sonic we registered, tell the game to use our generic model file.
+            // This signals that a custom BEWLR will handle the rendering.
+            SonicManager.INSTANCE.getDefinitions().keySet().forEach(id -> {
+                event.register(new ResourceLocation(id.getNamespace(), "item/" + id.getPath()));
+            });
+        }
+
+        @SubscribeEvent
+        public static void onModelBake(ModelEvent.BakingCompleted event) {
+            // This is the crucial step that was missing.
+            // It tells our PackAssetManager to "bake" all the loaded models
+            // so they can be rendered in-game.
+            PackAssetManager.INSTANCE.onBake(event.getModelBakery(), event.getModelManager());
+        }
+    }
+
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+    public static class ForgeEvents {
+        @SubscribeEvent
+        public static void onRegisterCommands(RegisterCommandsEvent event) {
+            ReloadPacksCommand.register(event.getDispatcher());
+        }
+
+        @SubscribeEvent
+        public static void onServerStarting(ServerStartingEvent event) {
+            LOGGER.info("DalekAPI server is starting: {}", event.getServer().getServerModName());
+        }
+    }
 }
+
--- src/main/java/wcore/sapphic/datapack/definition/DalekDefinition.java
+++ src/main/java/wcore/sapphic/datapack/definition/DalekDefinition.java
@@ -2,6 +2,7 @@ package wcore.sapphic.datapack.definition;
 
 import com.google.gson.annotations.SerializedName;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.MobCategory;
 import org.jetbrains.annotations.Nullable;
 
 public final class DalekDefinition {
@@ -15,10 +16,14 @@ public final class DalekDefinition {
     @SerializedName("spawn_egg")
     private final SpawnEggDefinition spawnEgg;
 
-    private DalekDefinition(String displayName, String texture, SpawnEggDefinition spawnEgg) {
+    @SerializedName("mob_category")
+    private final String mobCategory;
+
+    private DalekDefinition(String displayName, String texture, SpawnEggDefinition spawnEgg, String mobCategory) {
         this.displayName = displayName;
         this.texture = texture;
         this.spawnEgg = spawnEgg;
+        this.mobCategory = mobCategory;
     }
 
     public String getDisplayName() {
@@ -30,9 +35,22 @@ public final class DalekDefinition {
         return texture != null ? new ResourceLocation(texture) : null;
     }
 
-    @Nullable
     public SpawnEggDefinition getSpawnEgg() {
-        return spawnEgg;
+        // Return a default if null to prevent crashes
+        return spawnEgg != null ? spawnEgg : new SpawnEggDefinition();
+    }
+
+    public MobCategory getMobCategory() {
+        if (mobCategory == null || mobCategory.isBlank()) {
+            return MobCategory.MONSTER; // Default value
+        }
+        try {
+            // Converts the string from JSON (e.g., "monster") to the correct enum
+            return MobCategory.valueOf(mobCategory.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            // If the user enters an invalid string, fall back to the default
+            return MobCategory.MONSTER;
+        }
     }
 }
 
